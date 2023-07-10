6475dc9150d465bd54a127d1ae99f2ef0239f608
antoniogisondi <anto.gisondi@tiscali.it>
2023-07-10 10:17:45 +0200

    Basic exercise done

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php 
-    class Genere {
-        public $nome;
-
-        public function __construct($nome){
-            $this->nome = $nome;
-        }
-    }
-
     class Movie {
         public $titolo;
         public $genere;
@@ -14,11 +6,10 @@
         public $voto;
         public $durata;
 
-        public function __construct($titolo, Genere $genere, $anno, $voto, $durata ){
+        public function __construct($titolo, $genere, $anno, $durata){
             $this->titolo = $titolo;
             $this->genere = $genere;
             $this->anno = $anno;
-            $this->voto = $voto;
             $this->durata = $durata;
         }
         
@@ -32,18 +23,26 @@
         }
     }
 
-    
+    $pacific_rim = new Movie('Pacific Rim: La Rivolta', 'Azione', '2018', 150 );
+    $pacific_rim->voto = 7.5;
+
+    $ali = new Movie('ALI', 'Drammatico', '2001', 156);
+    $ali->voto = 8.5;
+
+    echo $pacific_rim->getMovies();
+    echo "<br/>";
+    echo $ali->getMovies()
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
 </html>
